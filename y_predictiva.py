47835f94847759ca0095949ce595ca8b0c901c4a
unknown <tic-320019@utnay.edu.mx>
2026-01-27 09:34:36 -0600

    TAREA ESCRITA EN Y_PREDICTIVA

--- y_predictiva.py
+++ y_predictiva.py
@@ -1,13 +1,17 @@
+#TAREA, CAMBIAR EL FOR DE AQUI POR CUALQUIER OTRO METODO QUE NO SEA NUMPY Y DE EL MISMO RESULTADO
+#TAMBIEN AGREAR E IMPORTAR LOS ELEMENTOS AL MAIN CUANDO YA TODO ESTE FUNCIONANDO POR SEPARADO, PRACTICAMENTE TERMINARLO
+#aAGREGAR EL MODULO L AL FINAL DE LAS DIAPOSITIVAS
 
-import values_xwyalpha as values
 
-#!Esto corresponde a cuanto espero que aprenda realmente el pokémon despues de los combates
-y_hat = []
+# import values_xwyalpha as values
 
-for i in range(3):          # 3 filas
-    valor = 0
-    for j in range(2):      # 2 columnas
-        valor = valor + values.X[i][j] * values.w[j]
-    y_hat.append(valor)
+# #!Esto corresponde a cuanto espero que aprenda realmente el pokémon despues de los combates
+# y_hat = []
 
-#print("y_hat =", y_hat)
+# for i in range(3):          # 3 filas
+#     valor = 0
+#     for j in range(2):      # 2 columnas
+#         valor = valor + values.X[i][j] * values.w[j]
+#     y_hat.append(valor)
+
+# #print("y_hat =", y_hat)
